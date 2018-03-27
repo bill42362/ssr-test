d8440204b9e61d565fd638c510d84bac591cd182
Bill42362 <bill42362@gmail.com>
2018-03-28 00:26:33 +0800

    Add some route in App.

--- src/client/js/component/App.jsx
+++ src/client/js/component/App.jsx
@@ -1,11 +1,20 @@
 // App.jsx
 'use strict';
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './Home.jsx';
 import '../../css/app.less';
 
 export const App = (props) => (
   <div className='app'>
-    App
+    <nav className='main-nav'>
+      <Link to='/'>Home</Link>
+      <Link to='/mainNav1'>MainNav1</Link>
+      <Link to='/mainNav2'>MainNav2</Link>
+    </nav>
+    <Switch>
+      <Route path='/' exact component={Home} />
+    </Switch>
   </div>
 );
 
---
+++ src/client/js/component/Home.jsx
@@ -0,0 +1,12 @@
+// Home.jsx
+'use strict';
+import React from 'react';
+import '../../css/home.less';
+
+export const Home = (props) => (
+  <div className='home'>
+    Home
+  </div>
+);
+
+export default Home;
